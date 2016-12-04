6b932d67905a81b6e2e72318c8d33b88214e3322
Chris Bala <chrbala@gmail.com>
2016-12-04 00:35:21 -0800

    Refactor wrapper tests

--- src/validate.test.js
+++ src/validate.test.js
@@ -1,21 +1,13 @@
 // @flow
 
 import test from 'ava';
+
 import { 
 	combineReducers, 
 	combineReducersAsync, 
 } from './validate';
 
-import {
-	NonNull, 
-	Permissive,
-	PermissiveAsync,
-} from './wrappers';
-
-import {
-	EXTRA_KEY_TEXT,
-	MISSING_KEY_TEXT,
-} from './strings';
+import { EXTRA_KEY_TEXT } from './strings';
 
 const IS_STRING_ERROR = 'Must be string';
 const isString = value => typeof value == 'string'
@@ -62,15 +54,6 @@ test('Top level undefined passes', t => {
 	});
 	const actual = validate(undefined);
 	const expected = null;
-	t.deepEqual(actual, expected);
-});
-
-test('Top level undefined NonNull does not pass', t => {
-	const validate = NonNull(combineReducers({
-		key: isString,
-	}));
-	const actual = validate(undefined);
-	const expected = MISSING_KEY_TEXT;
 	t.deepEqual(actual, expected);
 });
 
@@ -170,32 +153,6 @@ test('Reruns fragments of complex data types when data changes', t => {
 	});
 })();
 
-(() => {
-	const validate = combineReducers({
-		key: isString,
-		key2: NonNull(isString),
-	});
-
-	test('Missing normal property', t => {
-		const actual = validate({
-			key2: 'value',
-		});
-		const expected = null;
-		t.deepEqual(actual, expected);
-	});
-
-	test('Missing NonNull property', t => {
-		const actual = validate({
-			key: 'value',
-			key2: null,
-		});
-		const expected = {
-			key2: MISSING_KEY_TEXT,
-		};
-		t.deepEqual(actual, expected);
-	});
-})();
-
 test('Unexpected property', t => {
 	const validate = combineReducers({
 		key: isString,
@@ -210,36 +167,6 @@ test('Unexpected property', t => {
 	const expected = {
 		key3: EXTRA_KEY_TEXT,
 	};
-	t.deepEqual(actual, expected);
-});
-
-test('Unexpected property on permissive type', t => {
-	const validate = Permissive(combineReducers({
-		key: isString,
-		key2: isString,
-	}));
-
-	const actual = validate({
-		key: 'value',
-		key2: 'value',
-		key3: 'value',
-	});
-	const expected = null;
-	t.deepEqual(actual, expected);
-});
-
-test('Unexpected property on async permissive type', async t => {
-	const validateAsync = PermissiveAsync(combineReducersAsync({
-		key: isString,
-		key2: isString,
-	}));
-
-	const actual = await validateAsync({
-		key: 'value',
-		key2: 'value',
-		key3: 'value',
-	});
-	const expected = null;
 	t.deepEqual(actual, expected);
 });
 
@@ -390,17 +317,5 @@ test('Promises on keys should percolate up to combineReducersAsync', t => {
 		combineReducers({
 			key: async () => null,
 		});
-	});
-});
-
-test('NonNull must be used on a sync reducer', t => {
-	t.throws(() => {
-		NonNull(async () => null);
-	});
-});
-
-test('Permissive must be used on a sync reducer', t => {
-	t.throws(() => {
-		Permissive(async () => null);
 	});
 });
---
+++ src/wrappers.test.js
@@ -0,0 +1,98 @@
+// @flow
+
+import test from 'ava';
+
+import { 
+	combineReducers, 
+	combineReducersAsync, 
+} from './validate';
+
+import {
+	NonNull, 
+	Permissive,
+	PermissiveAsync,
+} from './wrappers';
+
+import { MISSING_KEY_TEXT } from './strings';
+
+const IS_STRING_ERROR = 'Must be string';
+const isString = value => typeof value == 'string'
+	? null
+	: IS_STRING_ERROR;
+
+test('Top level undefined NonNull does not pass', t => {
+	const validate = NonNull(combineReducers({
+		key: isString,
+	}));
+	const actual = validate(undefined);
+	const expected = MISSING_KEY_TEXT;
+	t.deepEqual(actual, expected);
+});
+
+(() => {
+	const validate = combineReducers({
+		key: isString,
+		key2: NonNull(isString),
+	});
+
+	test('Missing normal property', t => {
+		const actual = validate({
+			key2: 'value',
+		});
+		const expected = null;
+		t.deepEqual(actual, expected);
+	});
+
+	test('Missing NonNull property', t => {
+		const actual = validate({
+			key: 'value',
+			key2: null,
+		});
+		const expected = {
+			key2: MISSING_KEY_TEXT,
+		};
+		t.deepEqual(actual, expected);
+	});
+})();
+
+test('Unexpected property on permissive type', t => {
+	const validate = Permissive(combineReducers({
+		key: isString,
+		key2: isString,
+	}));
+
+	const actual = validate({
+		key: 'value',
+		key2: 'value',
+		key3: 'value',
+	});
+	const expected = null;
+	t.deepEqual(actual, expected);
+});
+
+test('Unexpected property on async permissive type', async t => {
+	const validateAsync = PermissiveAsync(combineReducersAsync({
+		key: isString,
+		key2: isString,
+	}));
+
+	const actual = await validateAsync({
+		key: 'value',
+		key2: 'value',
+		key3: 'value',
+	});
+	const expected = null;
+	t.deepEqual(actual, expected);
+});
+
+test('NonNull must be used on a sync reducer', t => {
+	t.throws(() => {
+		NonNull(async () => null);
+	});
+});
+
+test('Permissive must be used on a sync reducer', t => {
+	t.throws(() => {
+		Permissive(async () => null);
+	});
+});
