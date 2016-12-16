49f7642c5b66b6aef8a48fde66ef3234dab6ccf1
Chris Bala <chrbala@gmail.com>
2016-12-16 00:33:15 -0800

    Fill in createOperator tests

--- src/createOperator.test.js
+++ src/createOperator.test.js
@@ -2,6 +2,55 @@
 
 import test from 'ava';
 
-test.todo('Can create an operator');
-test.todo('Can create an operator with multiple reducers');
-test.todo('Can create an operator with sparse reducers');
+import createOperator from './createOperator';
+
+test('Can create an operator', t => {
+	const VALUE = 'VALUE';
+	const operator = createOperator('operator')({
+		flattener: {
+			operator: () => () => VALUE,
+		},
+	});
+	const { flattener } = operator();
+	t.is(flattener(), VALUE);
+});
+
+test('Can create an operator with multiple reducers', t => {
+	t.plan(1);
+
+	const reduce = {
+		operator: () => () => null,	
+	};
+	const reducer = {
+		flattener: reduce,
+	};
+	const operator = createOperator('operator')({
+		flattener: {
+			operator: (...reducers) => () => 
+				t.deepEqual(reducers, [reduce, reduce, reduce])
+			,
+		},
+	});
+	const { flattener } = operator(reducer, reducer, reducer);
+	flattener();
+});
+
+test('Can create an operator with sparse reducers', t => {
+	t.plan(1);
+
+	const reduce = {
+		operator: () => () => null,	
+	};
+	const reducer = {
+		flattener: reduce,
+	};
+	const operator = createOperator('operator')({
+		flattener: {
+			operator: (...reducers) => () => 
+				t.deepEqual(reducers, [reduce, reduce, reduce])
+			,
+		},
+	});
+	const { flattener } = operator(reducer, {}, reducer, {}, reducer);
+	flattener();
+});
