b9eec87042851d7ef1b21f793e156bf42fc7f20f
Chris Bala <chrbala@gmail.com>
2016-12-19 20:38:11 -0800

    Line length fix

--- src/createOperator.js
+++ src/createOperator.js
@@ -1,21 +1,26 @@
 // @flow
 
-import type { AllFlattenerType, AllReducerType, AnyFnType } from './shared/types';
+import type { 
+	AllFlattenerType, 
+	AllReducerType, 
+	AnyFnType,
+} from './shared/types';
 
 export default (operator: string) => 
-	(flatteners: AllFlattenerType<*>) => (...reducers: Array<AllReducerType<*>>) => {
-		const out: {[key: $Keys<typeof flatteners>]: AnyFnType} = {};
-		for (const flattenerName in flatteners) {
-			const scopedReducers = reducers.map(reducer => reducer[flattenerName]);
-			const reducerContexts = reducers;
-			
-			const create = flatteners[flattenerName][operator];
-			out[flattenerName] = create
-				? create(...scopedReducers)(...reducerContexts)
-				: () => console.warn(
-						new Error(`${flattenerName}/${operator} was not found`)
-					)
-			;		
-		}
-		return out;
-	};
+	(flatteners: AllFlattenerType<*>) => 
+		(...reducers: Array<AllReducerType<*>>) => {
+			const out: {[key: $Keys<typeof flatteners>]: AnyFnType} = {};
+			for (const flattenerName in flatteners) {
+				const scopedReducers = reducers.map(reducer => reducer[flattenerName]);
+				const reducerContexts = reducers;
+				
+				const create = flatteners[flattenerName][operator];
+				out[flattenerName] = create
+					? create(...scopedReducers)(...reducerContexts)
+					: () => console.warn(
+							new Error(`${flattenerName}/${operator} was not found`)
+						)
+				;		
+			}
+			return out;
+		};
