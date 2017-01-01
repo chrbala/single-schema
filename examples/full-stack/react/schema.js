572afd3cd80261b77cc962339498c0bd7dfd0529
Chris Bala <chrbala@gmail.com>
2017-01-01 09:15:21 -0800

    Move simple react example to its own folder

---
+++ examples/full-stack/react/schema.js
@@ -0,0 +1,15 @@
+// @flow
+
+import { combineReducers, array } from '../../src';
+import { string } from '../shared/schema';
+
+export const person = combineReducers({
+	name: string,
+});
+
+export const people = array(person);
+
+export const family = combineReducers({
+	adults: people,
+	children: people,
+});
